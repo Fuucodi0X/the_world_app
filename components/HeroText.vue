f6264226ed38c5b81746f1f46f99096a09f427db
Fuucodi0X <akmelf20@gmail.com>
2024-05-15 18:26:09 +0300

    created hero text

---
+++ components/HeroText.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col items-center">
+      <div class="flex flex-col font-poetsen items-center">
+        <h1 class="text-[65px] font-bold mb-4">
+          CountryQuest
+        </h1>
+        <span class="self-start ml-[120px] text-[60px]">&#x201e;</span>
+        <p class="text-[45px] font-extralight text-center">
+        Unveiling Nations, One Click at a Time
+        </p>
+        <span class="self-end mr-[120px] text-[60px]">&#x201d;</span>
+      </div>
+    </div>
+</template>
